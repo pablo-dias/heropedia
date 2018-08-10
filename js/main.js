f10658e9f8154544a540e1b161dda593e32f1242
Pablo Dias <pablo.dias.f@icloud.com>
2018-08-09 23:55:38 -0300

    Inserção do JS e alteração no visual

--- js/main.js
+++ js/main.js
@@ -1,2 +1,90 @@
+/*** Criando variavel o que o usuário digitar ***/
 const heroName = document.querySelector('#hero_name')
-const heroInfo = document.querySelector('.heroInfo')
+
+/***Selecionando o botão */
+const botao = document.querySelector('#searchButton')
+
+/**Selecionando o campo da imagem */
+let foto = document.querySelector('#foto')
+
+/**Selecionando o campo onde vai ser descrito as informações do heroi */
+let texto = document.querySelector('#texto')
+
+/**Criando padrão para validar a entrada */
+var regExp = /^([a-z]+\s[a-z]+)+$|^[a-z]+-[a-z]+$|^[a-z]+$/gi;
+/**Funçao que recebe o valor que o usuário digita, percorre o json com nome e id dos
+ herois, verificando se o valor corresponde a algum nome na lista. Em caso positivo,
+ retorna o id dos herois chamando a função algumacoisa*/
+
+const outracoisa = (a) => {
+    fetch('js/herois.json')
+        .then(res => res.json())
+        .then(herois => {
+            const h1 = Object.entries(herois)
+            let number = 0
+            h1.map( e => {if(e[0]== a){
+                number = e[1]
+                algumacoisa(number)
+            }
+            })
+        })
+} 
+
+/**Chama a função outracoisa ao botão ser clicado */
+botao.addEventListener('click', function() {
+    if(regExp.test(heroName.value) === true){
+        outracoisa(heroName.value)
+    }
+    else{
+        alert('Valor inválido, tenta de novo? Você pode digitar algo como "Super man" ou "A-Bomb" ou ainda "Batman". Valores como "Homem Aranha 2" não funcionam, beleza?')
+    }
+    
+})
+
+/**Passa o id recebido para a url da api, e a retorna */
+const algumacoisa = (number) => { 
+    
+    fetch(`http://www.superheroapi.com/api.php/2700278850112813/${number}/`)
+    .then(res => res.json())
+    .then(heroi => {
+        
+        texto.innerHTML = 
+        `<div class="heroInfo">
+            <ul class="infoList">
+            <h5>${heroi.name}</h5>
+            <li>Powerstats:</li>
+            <li>Intelligence: ${heroi.powerstats.intelligence}</li>
+            <li>Strength: ${heroi.powerstats.strength} </li>
+            <li>Speed: ${heroi.powerstats.speed}</li>
+            <li>Durability: ${heroi.powerstats.durability}</li>
+            <li>Power: ${heroi.powerstats.power}</li>
+            <li>Combat: ${heroi.powerstats.combat}</li>
+            <li>Biography:	</li>
+            <li>Full-name: ${heroi.biography["full-name"]}</li>
+            <li>Alter-egos: ${heroi.biography["alter-egos"]}</li>
+            <li>Place-of-birth: ${heroi.biography["place-of-birth"]}</li>
+            <li>First-appearance:	${heroi.biography["first-appearance"]}</li>
+            <li>Alignment: ${heroi.biography.alignment}</li>
+            <li>Appearance:</li>
+            <li>Gender: ${heroi.appearance.gender}</li>
+            <li>Race: ${heroi.appearance.race}</li>
+            <li>Height: ${heroi.appearance.height[1]}</li>
+            <li>Weight: ${heroi.appearance.weight[1]}</li>
+            <li>Work:	        </li>
+            <li>Occupation: ${heroi.work.occupation}</li>
+            <li>Base:	${heroi.work.base}</li>
+            <li>Connections:	</li>
+            <li>Group-affiliation: ${heroi.connections["group-affiliation"]}</li>
+            <li>Relatives: ${heroi.connections.relatives}</li>
+            </ul>
+        </div>`
+
+                            
+                           
+        foto.innerHTML = `<div class="heroImage" id="foto">
+                            <img src="${heroi.image.url}" alt="">
+                          </div>`
+        
+        
+    }) 
+}
